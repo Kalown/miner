c951161afbb4f09f19718e63f8e10a3445fabfe5
KALKI <86701829+Kalown@users.noreply.github.com>
2023-10-20 23:59:26 +0530

    Update email_spider.py
    ADDID BANNER

--- MINER/email_scraper/spiders/email_spider.py
+++ MINER/email_scraper/spiders/email_spider.py
@@ -1,14 +1,40 @@
 import scrapy
 import re
 
+import ssl
+
+
+print("""\
+               __                               
+              /  |                              
+ _____  ____  $$/  _______    ______    ______  
+/     \/    \ /  |/       \  /      \  /      \ 
+$$$$$$ $$$$  |$$ |$$$$$$$  |/$$$$$$  |/$$$$$$  |
+$$ | $$ | $$ |$$ |$$ |  $$ |$$    $$ |$$ |  $$/ 
+$$ | $$ | $$ |$$ |$$ |  $$ |$$$$$$$$/ $$ |      
+$$ | $$ | $$ |$$ |$$ |  $$ |$$       |$$ |      
+$$/  $$/  $$/ $$/ $$/   $$/  $$$$$$$/ $$/       
+------------------------------------------                                               
+------------------------------------------""")
+
+ctx = ssl.create_default_context() #ignore ssl certification error with this 3 lines
+ctx.check_hostname = False
+ctx.verify_mode= ssl.CERT_NONE
+
+
 class EmailSpider(scrapy.Spider):
     name = "email_spider"
-    start_urls = ["https://brave.com"]  # Replace with the starting URL
+
+    def start_requests(self):
+        user_input = input("Enter the starting URL: ")
+        self.start_urls = [user_input]
+
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         # Extract email addresses using regular expressions
         email_pattern = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}'
-        #email_pattern = r'[A-Za-z0-9._%+-]+@+"brave.com"'
         emails = re.findall(email_pattern, response.text)
 
         for email in emails:
@@ -20,4 +46,3 @@ class EmailSpider(scrapy.Spider):
         for next_page in response.css('a::attr(href)').extract():
             if next_page:
                 yield response.follow(next_page, self.parse)
-
